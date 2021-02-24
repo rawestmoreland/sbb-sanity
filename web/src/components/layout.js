d5ddc818335e5fa5cbf05b748d156253387c5e18
Richard Westmoreland <kegtrack@gmail.com>
2021-02-24 13:17:27 -0600

    Add seo tools

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import Header from './header'
+import React from "react"
+import Header from "./header"
 
-import '../styles/layout.css'
-import styles from './layout.module.css'
+import "../styles/layout.css"
+import styles from "./layout.module.css"
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle, subTitle}) => (
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle, subTitle }) => (
   <>
-    <Header siteTitle={siteTitle} subTitle={subTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
+    <Header
+      siteTitle={siteTitle}
+      subTitle={subTitle}
+      onHideNav={onHideNav}
+      onShowNav={onShowNav}
+      showNav={showNav}
+    />
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
         <div className={styles.siteInfo}>
-          &copy; {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a>{' '}
+          &copy; {new Date().getFullYear()}, Built with <a href="https://www.sanity.io">Sanity</a>{" "}
           &amp;
           {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </div>
       </div>
     </footer>
