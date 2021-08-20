3d7546c55e03b602784225bd59ddf5025ea19115
Richard Westmoreland <kegtrack@gmail.com>
2021-08-20 11:03:31 -0500

    add analytics tags

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,19 +1,26 @@
 // Load variables from `.env` as soon as possible
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV || "development"}`,
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
 })
 
-const clientConfig = require("./client-config")
+const clientConfig = require('./client-config')
 
-const isProd = process.env.NODE_ENV === "production"
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
-      resolve: "gatsby-source-sanity",
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [process.env.GOOGLE_GA, process.env.GOOGLE_UNIVERSAL],
+      },
+    },
+    {
+      resolve: 'gatsby-source-sanity',
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
